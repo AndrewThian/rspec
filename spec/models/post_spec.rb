82675c4c7af8a622d59307b3f80eadc19e392ed4
AndrewThian <andrewbobbythian@gmail.com>
2017-04-16 01:27:48 +0800

    create one_many post association with user

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe "Creation" do
-    before do
-      @post = Post.create(date: Date.today, rationale: "Something")
-    end
-    it "can be created" do
-      expect(@post).to be_valid
-    end
+  	before do
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+  		@post = Post.create(user_id: @user.id, date: Date.today, rationale: "Anything")
+  	end
 
-    it "cannot be created without a date and rationale" do
-      @post.date = nil
-      @post.rationale = nil
-      expect(@post).to_not be_valid
-    end
+  	it 'can be created' do
+  		expect(@post).to be_valid
+  	end
+
+  	it 'cannot be created without a date and rationale' do
+  		@post.date = nil
+  		@post.rationale = nil
+  		expect(@post).to_not be_valid
+  	end
   end
 end
